db15f46128ea8bd4c0137895eb8e8249c42befe0
Robert Coe <chakrawc@gmail.com>
2021-06-23 08:40:54 -0700

    allow custom content types

--- index.ts
+++ index.ts
@@ -3,28 +3,38 @@
 import { get, set } from "./store.ts";
 
 addEventListener("fetch", async (event: FetchEvent) => {
-  const { pathname, searchParams } = new URL(event.request.url);
+  const { pathname } = new URL(event.request.url);
 
   const key = pathname.slice(1);
   const existingContent = get(key);
 
   if (event.request.method === "GET") {
-    if (typeof existingContent !== "string") {
+    if (!existingContent) {
       return event.respondWith(new Response("not found", { status: 404 }));
     } else {
-      return event.respondWith(new Response(existingContent));
+      return event.respondWith(
+        new Response(existingContent.content, {
+          headers: { "Content-Type": existingContent.contentType },
+        })
+      );
     }
   } else if (event.request.method === "POST") {
     if (existingContent) {
       return event.respondWith(new Response("already exists", { status: 400 }));
     }
+
     const content = await event.request.text();
     // Limit to 32KiB
     if (content.length > 32 * 1024)
       return event.respondWith(new Response("too large", { status: 400 }));
 
-    set(key, content);
-    return event.respondWith(new Response(content));
+    const contentType =
+      event.request.headers.get("Content-Type") ?? "text/plain";
+
+    set(key, content, contentType);
+    return event.respondWith(
+      new Response(content, { headers: { "Content-Type": contentType } })
+    );
   }
 
   return event.respondWith(new Response("not found", { status: 404 }));
--- store.ts
+++ store.ts
@@ -1,15 +1,17 @@
-const store: Record<string, string | undefined> = {};
+type Article = { content: string; contentType: string };
+
+const store: Record<string, Article | undefined> = {};
 
 const channel = new BroadcastChannel("store");
 
 channel.onmessage = (event: MessageEvent) => {
-  const { action, key, content } = JSON.parse(event.data);
-  if (action === "add") store[key] = content;
+  const { action, key, content, contentType } = JSON.parse(event.data);
+  if (action === "add") store[key] = { content, contentType };
 };
 
-export const set = (key: string, content: string) => {
-  store[key] = content;
-  channel.postMessage({ action: "add", key, content });
+export const set = (key: string, content: string, contentType: string) => {
+  store[key] = { content, contentType };
+  channel.postMessage({ action: "add", key, content, contentType });
 };
 
-export const get = (key: string): string | undefined => store[key];
+export const get = (key: string): Article | undefined => store[key];
